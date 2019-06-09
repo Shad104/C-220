a562bc8075cedd00eaceb91e51d4a0391bc9c38b
Shad104 <49768826+Shad104@users.noreply.github.com>
2019-06-09 11:44:55 -0700

    Home Work6

---
+++ HomeWork6/HomeWork6/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeWork6.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.BirthdayCardContent birthdayCardContent)
+        {
+            if (ModelState.IsValid)
+                return View("BirthdayCardSent", birthdayCardContent);
+            else
+                return View();
+        }
+    }
+}
